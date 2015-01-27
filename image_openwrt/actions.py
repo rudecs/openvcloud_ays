41879e4d1822146d88d78cfe5c453087b5726aa7
Jo De Boeck <deboeck.jo@gmail.com>
2015-01-27 14:50:58 +0200

    Add image_openwrt

---
+++ image_openwrt/actions.py
@@ -0,0 +1,29 @@
+from JumpScale import j
+
+ActionsBase=j.packages.getActionsBaseClass()
+
+class Actions(ActionsBase):
+    """
+    process for install
+    -------------------
+    step1: prepare actions
+    step2: check_requirements action
+    step3: download files & copy on right location (hrd info is used)
+    step4: configure action
+    step5: check_uptime_local to see if process stops  (uses timeout $process.stop.timeout)
+    step5b: if check uptime was true will do stop action and retry the check_uptime_local check
+    step5c: if check uptime was true even after stop will do halt action and retry the check_uptime_local check
+    step6: use the info in the hrd to start the application
+    step7: do check_uptime_local to see if process starts
+    step7b: do monitor_local to see if package healthy installed & running
+    step7c: do monitor_remote to see if package healthy installed & running, but this time test is done from central location
+    """
+
+
+    def configure(self, **kwargs):
+        from CloudscalerLibcloud.imageutil import registerImage
+        name = 'OpenWRT 14.07'
+        imagename = 'openwrt1407.qcow2'
+        registerImage(self.jp_instance, name, imagename, 'Linux', 10)
+
+
