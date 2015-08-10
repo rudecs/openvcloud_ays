bfdb587e5999ac7085cd9c41da5f739a4efd4f1b
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-08-10 12:31:46 +0200

    add bootstrapp service
    see
    https://git.aydo.com/0-complexity/openvcloud/commit/b9530bd954a226eb58d35ceb2a6f37d680d7ce7c

---
+++ bootstrapp/actions.py
@@ -0,0 +1,20 @@
+from JumpScale import j
+import requests
+
+ActionsBase=j.atyourservice.getActionsBaseClass()
+
+
+class Actions(ActionsBase):
+
+    def prepare(self, serviceObj):
+        path = '$(system.paths.base)/apps/bootstrapp'
+        if not j.system.fs.exists(path=path):
+            j.system.fs.createDir(path)
+
+    def configure(self, serviceObj):
+        from ipdb import set_trace;set_trace()
+        ps = serviceObj.hrd.getDictFromPrefix('service.process')['1']
+        args = ps['args']
+        args = '--gitpath $(instance.ovc_git) --hrd %s' % serviceObj.hrd.path
+        ps['args'] = args
+        # serviceObj.hrd.save()
