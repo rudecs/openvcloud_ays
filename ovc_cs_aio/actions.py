7d5b75fb99bc456aac4b2254d7f5c234ee41ef7c
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-08-14 10:06:16 +0200

    don't user hardcoded value for repoPath

--- ovc_cs_aio/actions.py
+++ ovc_cs_aio/actions.py
@@ -12,7 +12,7 @@ class Actions(ActionsBase):
 
         def reflector():
             # install reflector
-            self.initReflectorVM(spacesecret, '$(instance.reflector.root.passphrase)', delete='$(instance.param.override)')
+            self.initReflectorVM(spacesecret, '$(instance.reflector.root.passphrase)', '$(instance.param.repo.path)', delete='$(instance.param.override)')
         j.actions.start(description='install reflector vm', action=reflector, category='openvlcoud', name='install_reflector', serviceObj=serviceObj)
 
         def proxy():
@@ -24,14 +24,14 @@ class Actions(ActionsBase):
         j.actions.start(description='install proxy vm', action=proxy, category='openvlcoud', name='install_proxy', serviceObj=serviceObj)
 
         def master():
-            # install 
-            self.initMasterVM(spacesecret, '$(instance.master.rootpasswd)', '$(instance.master.publicip.start)', 
+            # install
+            self.initMasterVM(spacesecret, '$(instance.master.rootpasswd)', '$(instance.master.publicip.start)',
                             '$(instance.master.publicip.end)', '$(instance.master.dcpm.url)',
                             '$(instance.ovs.url)', '$(instance.portal.url)', '$(instance.oauth.url)',
-                            '$(instance.defense.url)', delete='$(instance.param.override)')
+                            '$(instance.defense.url)', '$(instance.param.repo.path)', delete='$(instance.param.override)')
         j.actions.start(description='install master vm', action=master, category='openvlcoud', name='install_master', serviceObj=serviceObj)
 
-    def initReflectorVM(self, spacesecret, passphrase, delete=False):
+    def initReflectorVM(self, spacesecret, passphrase, repoPath, delete=False):
         """
         this methods need to be run from the ovc_git VM
 
@@ -66,10 +66,10 @@ class Actions(ActionsBase):
         cl.ssh_keygen('guest', keytype='rsa')
         # copy the key on the git vm
         keys = {
-            '/root/.ssh/id_rsa': '/opt/ovc_git/keys/reflector_root',
-            '/root/.ssh/id_rsa.pub': '/opt/ovc_git/keys/reflector_root.pub',
-            '/home/guest/.ssh/id_rsa': '/opt/ovc_git/keys/reflector_guest',
-            '/home/guest/.ssh/id_rsa.pub': '/opt/ovc_git/keys/reflector_guest.pub'
+            '/root/.ssh/id_rsa': '%s/keys/reflector_root' % repoPath,
+            '/root/.ssh/id_rsa.pub': '%s/keys/reflector_root.pub' % repoPath,
+            '/home/guest/.ssh/id_rsa': '%s/keys/reflector_guest' % repoPath,
+            '/home/guest/.ssh/id_rsa.pub': '%s/keys/reflector_guest.pub' % repoPath
         }
         for source, destination in keys.iteritems():
             content = cl.file_read(source)
@@ -99,7 +99,7 @@ class Actions(ActionsBase):
         # install bootrapp on git vm
         data = {
             'instance.listen.port': 5000,
-            'instance.ovc_git': '/opt/ovc_git',
+            'instance.ovc_git': repoPath,
             'instance.master.name': 'jumpscale__node.ssh__ovc_master',
             'instance.reflector.ip.priv': privIP,
             'instance.reflector.ip.pub': pubIP,
@@ -176,7 +176,7 @@ class Actions(ActionsBase):
         ssloffloader.consume('node', nodeService.instance)
         ssloffloader.install(reinstall=True, deps=True)
 
-    def initMasterVM(self, spacesecret, masterPasswd, publicipStart, publicipEnd, dcpmUrl, ovsUrl, portalUrl, oauthUrl, defenseUrl, delete=False):
+    def initMasterVM(self, spacesecret, masterPasswd, publicipStart, publicipEnd, dcpmUrl, ovsUrl, portalUrl, oauthUrl, defenseUrl, repoPath, delete=False):
         """
         this methods need to be run from the ovc_git VM
 
@@ -205,12 +205,12 @@ class Actions(ActionsBase):
         print 'generate keypair on the vm'
         cl.ssh_keygen('root', keytype='rsa')
         keys = {
-            '/root/.ssh/id_rsa': '/opt/ovc_git/master_root',
-            '/root/.ssh/id_rsa.pub': '/opt/ovc_git/master_root.pub',
+            '/root/.ssh/id_rsa': '%s/keys/master_root' % repoPath,
+            '/root/.ssh/id_rsa.pub': '%s/keys/master_root.pub' % repoPath,
         }
         for source, destination in keys.iteritems():
-            content = cl.file_read('/root/.ssh/id_rsa')
-            j.system.fs.writeFile(filename='/opt/ovc_git/master_root', contents=content)
+            content = cl.file_read(source)
+            j.system.fs.writeFile(filename=destination, contents=content)
 
         # install Jumpscale
         print "install jumpscale"
@@ -254,5 +254,5 @@ class Actions(ActionsBase):
         master.consume('node', nodeService.instance)
         master.install(reinstall=True, deps=True)
 
-        content = j.system.fs.fileGetContents('/opt/ovc_git/reflector_guest.pub')
+        content = j.system.fs.fileGetContents('%s/keys/reflector_guest.pub' % repoPath)
         cl.file_append('/root/.ssh/.ssh/authorized_keys', content)
