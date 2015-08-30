2a12b50d8b38dad13beab261b35205b5170b414e
David Shafik <david@codescalers.com>
2015-08-30 13:28:27 +0000

    update Ubuntu 15.04 image

--- image_ubuntu-1504-x64/actions.py
+++ image_ubuntu-1504-x64/actions.py
@@ -23,7 +23,7 @@ class Actions(ActionsBase):
     def configure(self, serviceObj):
         from CloudscalerLibcloud.imageutil import registerImage
         name = 'Ubuntu 15.04 x64'
-        imagename = 'Ubuntu.15.04.x64.qcow2'
+        imagename = 'Ubuntu.15.04.qcow2'
         registerImage(serviceObj, name, imagename, 'Linux', 10)
 
 
