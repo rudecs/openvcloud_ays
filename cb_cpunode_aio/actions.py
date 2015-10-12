ad51afb7af5ab7c16bf99dd2054a6a62209f55cd
Jo De Boeck <deboeck.jo@gmail.com>
2015-10-12 22:39:45 +0200

    Add cpunode and storage role to cb_cpunode_aio

--- cb_cpunode_aio/actions.py
+++ cb_cpunode_aio/actions.py
@@ -3,6 +3,15 @@ from JumpScale import j
 ActionsBase=j.atyourservice.getActionsBaseClass()
 
 class Actions(ActionsBase):
-    pass
+    def configure(self, serviceObject):
+        roles = j.application.config.getList('grid.node.roles')
+        change = False
+        for role in ['cpunode', 'storage']:
+            if role not in roles:
+                change = True
+                roles.append(role)
+        if change:
+            j.application.config.set('grid.node.roles', roles)
+            j.atyourservice.get(name='jsagent', instance='main').restart()
 
 
