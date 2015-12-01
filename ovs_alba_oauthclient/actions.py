b3864b7438c73856cd05a48629685592ed8579a9
Maxime Daniel <root@maxux.net>
2015-12-01 15:05:25 +0100

    improve ovs_alba_oauthclient with auto grabber

--- ovs_alba_oauthclient/actions.py
+++ ovs_alba_oauthclient/actions.py
@@ -4,4 +4,20 @@ ActionsBase = j.atyourservice.getActionsBaseClass()
 
 
 class Actions(ActionsBase):
-    pass
+    def configure(self, serviceObj):
+        masterService = j.application.getAppInstanceHRD(name='ovs_setup', instance='main', domain='openvcloud')
+        masterNodeName = masterService.getStr('instance.ovs.masternode')
+        
+        masterNode = j.atyourservice.get(name='node.ssh', instance=masterNodeName)
+        content = masterNode.execute('python /opt/code/git/0-complexity/openvcloud/scripts/ovs/alba-get-user.py')
+        
+        user = json.loads(content)
+        
+        if user.get('error'):
+            print '[-] cannot grab credentials: %s' % user['error']
+            return False
+        
+        serviceObj.hrd.set('instance.client_id', user['client'])
+        serviceObj.hrd.set('instance.client_secret', user['secret'])
+        
+        return True
