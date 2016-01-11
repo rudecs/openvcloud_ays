a3b1013a61036325db6a03649be3a69211d2a983
Jo De Boeck <deboeck.jo@gmail.com>
2016-01-11 15:48:02 +0200

    Use eugine 2.6.1

--- openvstorage/actions.py
+++ openvstorage/actions.py
@@ -24,7 +24,7 @@ class Actions(ActionsBase):
     DJANGO_SETTINGS = '/opt/OpenvStorage/webapps/api/settings.py'
 
     def installOVS(self):
-        release = 'eugene'
+        release = 'eugene-updates'
         packages = ['ntp', 'kvm', 'libvirt0', 'python-libvirt', 'virtinst', 'openvstorage-hc']
         j.system.fs.writeFile(filename="/etc/apt/sources.list.d/openvstorage.list", contents="deb http://apt.openvstorage.org %s main" % release, append=False)
         j.system.platform.ubuntu.updatePackageMetadata()
