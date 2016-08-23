d2ea31da2af37c313ea2a21b88272843281f25c3
Jo De Boeck <deboeck.jo@gmail.com>
2016-08-23 13:36:19 +0200

    Remove tests after install g8vdc

--- g8vdc/actions.py
+++ g8vdc/actions.py
@@ -1,7 +1,8 @@
 from JumpScale import j
 
-ActionsBase=j.atyourservice.getActionsBaseClass()
+ActionsBase = j.atyourservice.getActionsBaseClass()
 
 class Actions(ActionsBase):
-    """
-    """
+    def configure(self, serviceObject):
+        path = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'g8vdc', '.files', 'test')
+        j.system.fs.removeDirTree(path)
