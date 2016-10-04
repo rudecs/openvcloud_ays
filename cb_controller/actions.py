98547a07eb393ac3cdb3c33a7a01d707dff2f2f8
Jo De Boeck <deboeck.jo@gmail.com>
2016-10-04 13:57:46 +0200

    Check if container is running before pulling and creating it

--- cb_controller/actions.py
+++ cb_controller/actions.py
@@ -14,7 +14,23 @@ class Actions(ActionsBase):
         if change:
             j.application.config.set('grid.node.roles', roles)
             j.atyourservice.get(name='jsagent', instance='main').restart()
-        print('Pulling influxdb docker')
-        j.system.process.execute('docker run -td -p 8083:8083 -p 8086:8086 -v influxdb:/optvar/data/influxdb --name influxdb jumpscale/ubuntu1604_influxdb')
-        print('Pulling jsagent docker')
-        j.system.process.execute('docker run -td -p 9022:22 --name jsagent jumpscale/ubuntu1604_jsagent')
+        
+        def container_check(name):
+            import docker
+            client = docker.Client()
+            for container in client.containers():
+                for cname in container['Names']:
+                    if cname.lstrip('/') == name:
+                        if container['Status'].startswith('Up'):
+                            return True
+                        else:
+                            client.start(container['Id'])
+                            return True
+            return False
+
+        if not container_check('influxdb'):
+            print('Pulling influxdb docker')
+            j.system.process.execute('docker run -td -p 8083:8083 -p 8086:8086 -v influxdb:/optvar/data/influxdb --name influxdb jumpscale/ubuntu1604_influxdb')
+        if not container_check('jsagent'):
+            print('Pulling jsagent docker')
+            j.system.process.execute('docker run -td -p 9022:22 --name jsagent jumpscale/ubuntu1604_jsagent')
