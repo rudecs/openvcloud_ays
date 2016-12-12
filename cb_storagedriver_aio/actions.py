bc1443b0de0a65e1a6d79b1c70adfeed8a41850c
Jo De Boeck <deboeck.jo@gmail.com>
2016-12-12 13:50:22 +0200

    Add storagemaster role

--- cb_storagedriver_aio/actions.py
+++ cb_storagedriver_aio/actions.py
@@ -4,10 +4,14 @@ ActionsBase = j.atyourservice.getActionsBaseClass()
 
 
 class Actions(ActionsBase):
+
     def configure(self, serviceObject):
         roles = j.application.config.getList('grid.node.roles')
         change = False
-        for role in ['storagedriver']:
+        storageroles = ['storagedriver']
+        if 'MASTER' in j.system.process.execute('ovs config get "ovs/framework/hosts/$(cat /etc/openvstorage_id)/type"')[1]:
+            storageroles.append('storagemaster')
+        for role in storageroles:
             if role not in roles:
                 change = True
                 roles.append(role)
